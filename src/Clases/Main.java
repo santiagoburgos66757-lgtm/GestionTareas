b91b5c4dbf6f4003cde7565bcbb1291cdf986159
santiagoburgos66757-lgtm <santiago.burgos66757@ucaldas.edu.co>
2026-03-09 13:59:09 -0500

    Santiago Burgos

--- src/Clases/Main.java
+++ src/Clases/Main.java
@@ -6,9 +6,9 @@ void main() {
 
     GestorTareas gestor = new GestorTareas();
     gestor.agregarTarea(new Tarea(1,"tarea de matematicas",5,false));
-    gestor.agregarTarea(new Tarea(2,"barrer",2,false));
+    gestor.agregarTarea(new Tarea(2,"Organizar el cuarto",2,false));
     gestor.agregarTarea(new Tarea(3,"ir al gimnasio",5,false));
-    gestor.agregarTarea(new Tarea(4,"correr",3,false));
+    gestor.agregarTarea(new Tarea(4,"estudiar Arrays y likendList",3,false));
     gestor.agregarTarea(new Tarea(5,"sacar al perro",4,false));
     gestor.listarTareas();
     gestor.marcarCompletada(1);
